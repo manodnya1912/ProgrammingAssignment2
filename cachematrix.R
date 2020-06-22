0a4a58a69a54d124df07ecf68ca576684160ee50
manodnya1912 <67227204+manodnya1912@users.noreply.github.com>
2020-06-22 14:33:51 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,36 @@
-## Below created function itself creates a matrix which cache its inverse.
-
+# makes a matrix object with functions of setting a solved inverse, or getting the stored inverse
 makeCacheMatrix <- function(x = matrix()) {
-inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
-  getInverse <- function() inv
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-
-
+	inverse <- NULL
+	set <- function(y){
+		x <<- y
+		inverse <<- NULL
+	}
+	# get returns the x initially used to define makeCacheMatrix object
+	get <- function()x
+	# setInverse solves for the inverse of matrix x, then stores the solution to inverse variable within object
+	setInverse <- function(solve) inverse <<- solve
+	# getInverse returns the stored inverse value within object
+	getInverse <- function() inverse
+	#returns a list of functions to be associated with matrix object
+	list(set=set, get= get, setInverse = setInverse, getInverse = getInverse)	
 }
 
 
 ## Write a short comment describing this function
 
-## The function below computes the inverse of the matrix that is the output of makeCacheMatrix .
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-inv <- x$getInverse()
-  if(!is.null(inv)){
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv   
+	inverse <- x$getInverse()
+	# prints message and returns inverse if cached inverse has been calculated previously
+	if(!is.null(inverse){
+		print("getting cached inverse")
+		return(inverse)
+	}
+	# returns data initially used to make x(makeCacheMatrix object)
+	data <- x$get
+	# use inbuilt solve function to solve for inverse of data
+	inverse <- solve(data,...)
+	# use setInverse function within object to store solution of inverse calculated
+	x$setInverse(inverse)
+	inverse
 }
